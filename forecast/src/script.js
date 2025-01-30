07bc924bb2f66cc40728b35bb898aa3eeec842f7
Boitsholo Ramokhua <ramokhuaboitsholo@gmail.com>
2025-01-30 13:43:46 +0200

    updated

--- forecast/src/script.js
+++ forecast/src/script.js
@@ -42,7 +42,7 @@ function refreshWeather(response) {
   }
   
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "e6cc64taaa3ed8def581fc3fao0d0b1c";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
   }
@@ -62,7 +62,7 @@ function refreshWeather(response) {
   }
   
   function getForecast(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "e6cc64taaa3ed8def581fc3fao0d0b1c";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
     axios(apiUrl).then(displayForecast);
   }
